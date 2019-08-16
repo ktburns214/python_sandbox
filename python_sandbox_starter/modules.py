8b29a1bd6071e4d50d4368bd7adc9df4ba389763
kathleen.e.burns@gmail.com <kathleen.e.burns@gmail.com>
2019-08-16 01:56:21 -0500

    completed modules exercises

--- python_sandbox_starter/modules.py
+++ python_sandbox_starter/modules.py
@@ -1,1 +1,26 @@
-# A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+# A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django), as well as custom modules
+
+# import a core python module (i.e. datetime)
+import datetime
+today = datetime.date.today()
+print(today)
+
+# import a certain piece of a core python module (i.e. time)
+from time import time
+timestamp = time()
+print(timestamp)
+
+# pip module (i.e. camelcase)
+from camelcase import CamelCase 
+c = CamelCase()
+print(c.hump("hello world"))
+
+# import custom module (i.e. validator.py)
+import validator
+from validator import validate_email
+
+email = "test@test.com"
+if validate_email(email):
+    print("email is valid")
+else:
+    print("email is bad")
