e66b35a755fc98bff0725789195ba6c9fae18cae
kathleen.e.burns@gmail.com <kathleen.e.burns@gmail.com>
2019-08-15 23:58:22 -0500

    completed lists and strings exercises

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,50 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+# lists in python are similar to arrays
+
+# create list
+numbers = [1, 2, 3, 4, 5]
+
+fruits = ["apples", "oranges", "grapes", "strawberries"]
+
+# create list using a constructor
+numbers2 = list((6, 7, 8, 9))
+
+print(numbers, numbers2)
+
+# get a single value from list
+print(fruits[1])
+
+# get length
+print(len(fruits))
+
+# append to list
+fruits.append("mangos")
+print(fruits)
+
+# remove from list
+fruits.remove("grapes")
+print(fruits)
+
+# insert into position
+fruits.insert(2, "raspberries")
+print(fruits)
+
+# change a value
+fruits[0] = "blueberries"
+print(fruits)
+
+# remove with pop
+fruits.pop(2)
+print(fruits)
+
+# reverse list
+fruits.reverse()
+print(fruits)
+
+# sort list alphabetically
+fruits.sort()
+print(fruits)
+
+# reverse sort
+fruits.sort(reverse=True)
+print(fruits)
--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,60 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = "Katie"
+age = 29
+
+# concatenate
+print("Hello, my name is " + name + " and I am " + str(age))
 
 # String Formatting
+# positional arguments
+print("My name is {name} and I am {age}".format(name=name, age=age))
+
+# F-strings
+print(f"Hello, my name is {name} and I am {age}")
 
 # String Methods
+s = "hello world"
+
+# capitalize string
+print(s.capitalize())
+
+# make all uppercase
+print(s.upper())
+
+# make all lowercase
+print(s.lower())
+
+# swapcase
+print(s.swapcase())
+
+# get length
+print(len(s))
+
+# replace
+print(s.replace("world", "everyone"))
+
+# count--counts occurance of a character
+sub = "h"
+print(s.count(sub))
+
+# starts with--outputs true or false value
+print(s.startswith("hello"))
+
+# ends with--outputs true or false value
+print(s.endswith("d"))
+
+# slpit into list
+print(s.split())
+
+# find position of a character
+print(s.find("r"))
+
+# is all alphanumeric--true or false, includes spaces
+print(s.isalnum())
+
+# is all alphabetic--true or false, includes spaces
+print(s.isalpha())
+
+# is all numeric--true or false
+print(s.isnumeric())
